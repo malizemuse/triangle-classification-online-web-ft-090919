b1e3f4d9483ad2fd192bc38afead21bdb04d9828
gregarious-number-3828 <muse.malize@gmail.com>
2019-10-03 11:39:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,36 +1,34 @@
 class Triangle
-  
+
   attr_accessor :side_1, :side_2, :side_3
-  
+
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-  end 
-  
+  end
+
   def kind #returns symbol
-    
+
     # Check if triangle is valid
       # If invalid, raise TriangleError
-    # If valid then check for triangle type 
-      # :equilateral 
+    # If valid then check for triangle type
+      # :equilateral
       # :isosceles
       # :scalene
-    
-    
-    
-  end 
-  
+
+
+
+  end
+
   def valid?
     # sum of the lengths of any two sides of a triangle always exceeds the length of the third side
-    # each side must be larger than 0 
+    # each side must be larger than 0
     
-    
-  end 
-  
+
+  end
+
   # Create custom TriangleError here
-  
-  
+
+
 end
-
-
