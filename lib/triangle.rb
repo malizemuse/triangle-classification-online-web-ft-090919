79af67c6547a900be79929457ceb31a245c5f787
gregarious-number-3828 <muse.malize@gmail.com>
2019-10-03 11:43:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,10 +25,10 @@ triangle-classification-online-web-ft-090919
     # sum of the lengths of any two sides of a triangle always exceeds the length of the third side
     # each side must be larger than 0
 
-    if (@side_1 > 0 && @side_2 > 0 && @side_3 > 0 &&
+    if @side_1 > 0 && @side_2 > 0 && @side_3 > 0 &&
       (@side_1 + @side_2) > @side_3 &&
       (@side_2 + @side_3) > @side_1 &&
-      (@side_3 + @side_1) > @side_2)
+      (@side_3 + @side_1) > @side_2
       true
     else
       false
