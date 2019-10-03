8858c322d0a32f55a5ea8119301dbb4a9241083b
gregarious-number-3828 <muse.malize@gmail.com>
2019-10-03 11:51:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,7 @@ class Triangle
       if @side_1 == @side_2 && @side_2 == @side_3
         :equilateral
       elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
-        :scalene 
+        :scalene
       elsif (@side_1 == @side_2 && @side_1 != @side_3) ||
         (@side_2 == @side_3 && @side_2 != @side_1) ||
         (@side_1 == @side_3 && @side_1 != @side_2)
