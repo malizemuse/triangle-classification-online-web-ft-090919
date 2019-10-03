a5b46f8aa5251ce66ec20f3f1721e79c5de4ef9d
gregarious-number-3828 <muse.malize@gmail.com>
2019-10-03 11:42:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def kind #returns symbol
-
+triangle-classification-online-web-ft-090919
     # Check if triangle is valid
       # If invalid, raise TriangleError
     # If valid then check for triangle type
@@ -24,7 +24,15 @@ class Triangle
   def valid?
     # sum of the lengths of any two sides of a triangle always exceeds the length of the third side
     # each side must be larger than 0
-    
+
+    if @side_1 > 0 && @side_2 > 0 && @side_3 > 0 &&
+      (@side_1 + @side_2) > @side_3 &&
+      (@side_2 + @side_3) > @side_1 &&
+      (@side_3 + @side_1) > @side_2
+      true
+    else
+      false
+    end
 
   end
 
