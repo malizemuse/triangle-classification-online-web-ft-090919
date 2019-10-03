4e08ce881b1d381a66b809544b75811d50ee38ef
gregarious-number-3828 <muse.malize@gmail.com>
2019-10-03 12:26:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,55 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind #returns symbol
+
+    # Check if triangle is valid
+      # If invalid, raise TriangleError
+    # If valid then check for triangle type
+      # :equilateral
+      # :isosceles
+      # :scalene
+
+    if self.valid?
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
+        :scalene
+      elsif (@side_1 == @side_2 && @side_1 != @side_3) ||
+        (@side_2 == @side_3 && @side_2 != @side_1) ||
+        (@side_1 == @side_3 && @side_1 != @side_2)
+        :isosceles
+      end
+    else
+      raise TriangleError
+    end
+
+  end
+
+  def valid?
+    # sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+    # each side must be larger than 0
+
+    if @side_1 > 0 && @side_2 > 0 && @side_3 > 0 &&
+      (@side_1 + @side_2) > @side_3 &&
+      (@side_2 + @side_3) > @side_1 &&
+      (@side_3 + @side_1) > @side_2
+      true
+    else
+      false
+    end
+
+  end
+
+  # Create custom TriangleError here
+  class TriangleError < StandardError
+  end
+
 end
